7d7050d195608e7e7861a831cf033abbbf26e331
Deep64930 <deepakraj5455@gmail.com>
2022-11-19 20:57:30 +0530

    this is my first commit

---
+++ react-learning/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>You are Learning form control and uncontrol </h1>
+   
+    </div>
+  );
+}
+
+export default App;
